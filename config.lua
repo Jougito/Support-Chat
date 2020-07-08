1fc78d59fd3426083fbef26987a7cbf113eeb35d
Jougito - Kheros <jougito@hotmail.es>
2020-07-08 03:24:25 +0200

    Subida de archivos

---
+++ config.lua
@@ -0,0 +1,24 @@
+--------------------------
+-- Chat de Ayuda InGame --
+--     por Jougito      --
+--------------------------
+
+Config = {}
+
+Config.Command      = "a" -- Comando para la ayuda
+
+Label = {}
+
+Label.User          = "Ayuda" -- Título Usuario (user)
+Label.Support       = "Soporte" -- Título Soporte (helper)
+Label.SupportPlus   = "Moderador" -- Título Moderador (mod)
+Label.Moderator     = "Administrador" -- Título Administrador (admin)
+Label.Administrator = "Fundador" -- Título Fundador (superadmin)
+
+Color = {}
+
+Color.User          = { 255, 165, 0 } -- Color Usuario (Naranja)
+Color.Support       = { 255, 55, 255 } -- Color Soporte (Rosa)
+Color.SupportPlus   = { 255, 235, 0 } -- Color Soporte + (Amarillo)
+Color.Moderator     = { 100, 255, 55 } -- Color Moderador (Verde)
+Color.Administrator = { 55, 200, 255 } -- Color Administrador (Azul)
