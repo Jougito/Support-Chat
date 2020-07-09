ffbe2f2361bbe240dfbdf5ae2a8515413a0a8b2e
Jougito - Kheros <jougito@hotmail.es>
2020-07-09 07:03:48 +0200

    Corregido Code Style y Updater

--- server/main.lua
+++ server/main.lua
@@ -5,26 +5,26 @@
 
 RegisterCommand(Config.Command, function(source, args, rawCommand)
 
-	args = table.concat(args, ' ')
-	
-	local Rank = GetRank(source)
+    args = table.concat(args, ' ')
+    
+    local Rank = GetRank(source)
     local uID = source
     local uName = GetPlayerName(uID)
-	
-	if args ~= "" then
-		if(Rank == 1)then -- Soporte
-			TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.Support .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.Support })
-		elseif(Rank == 2)then -- Soporte +
-			TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.SupportPlus .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.SupportPlus })
-		elseif(Rank == 3)then -- Moderador+
-			TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.Moderator .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.Moderator })
-		elseif(Rank == 4)then -- Administrador
-			TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.Administrator .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.Administrator })
-		else -- Usuario normal
-			TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.User .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.User })
-		end
-	end
-	
+    
+    if args ~= "" then
+        if(Rank == 1)then -- Soporte
+            TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.Support .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.Support })
+        elseif(Rank == 2)then -- Soporte +
+            TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.SupportPlus .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.SupportPlus })
+        elseif(Rank == 3)then -- Moderador+
+            TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.Moderator .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.Moderator })
+        elseif(Rank == 4)then -- Administrador
+            TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.Administrator .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.Administrator })
+        else -- Usuario normal
+            TriggerClientEvent('chat:addMessage', -1, { args = { "[".. Label.User .."] ".. uName .." (".. uID ..")", "^7".. args .."" }, color = Color.User })
+        end
+    end
+    
 end, false)
 
 -- Funciones
@@ -43,28 +43,28 @@ end
 
 -- Version Checking - DON'T TOUCH THIS
 
-local CurrentVersion = '1.0.0'
+local CurrentVersion = '1.0.1'
 local GithubResourceName = 'Support-Chat'
 
 PerformHttpRequest('https://raw.githubusercontent.com/Jougito/FiveM_Resources/master/' .. GithubResourceName .. '/VERSION', function(Error, NewestVersion, Header)
-	PerformHttpRequest('https://raw.githubusercontent.com/Jougito/FiveM_Resources/master/' .. GithubResourceName .. '/CHANGELOG', function(Error, Changes, Header)
-		print('\n')
-        print('[Support Chat] Checking for updates...')
-        print('')
-        print('[Support Chat] Current version: ' .. CurrentVersion)
-        print('[Support Chat] Updater version: ' .. NewestVersion)
-        print('')
-		if CurrentVersion ~= NewestVersion then
-			print('[Support Chat] Your script is outdated!')
-			print('')
-            print('[Support Chat] CHANGELOG ' .. NewestVersion .. ':')
-            print('')
+    PerformHttpRequest('https://raw.githubusercontent.com/Jougito/FiveM_Resources/master/' .. GithubResourceName .. '/CHANGELOG', function(Error, Changes, Header)
+        print('^0')
+        print('^6[Support Chat]^0 Checking for updates...')
+        print('^0')
+        print('^6[Support Chat]^0 Current version: ^5' .. CurrentVersion .. '^0')
+        print('^6[Support Chat]^0 Updater version: ^5' .. NewestVersion .. '^0')
+        print('^0')
+        if CurrentVersion ~= NewestVersion then
+            print('^6[Support Chat]^0 Your script is ^8outdated^0!')
+            print('^0')
+            print('^6[Support Chat] ^3CHANGELOG ^5' .. NewestVersion .. ':^0')
+            print('^3')
             print(Changes)
-            print('')
-            print('[Support Chat] You are not running the newest stable version of Staff Chat. Please update: https://github.com/Jougito/Support-Chat')
-		else
-			print('[Support Chat] Your script is up-to-update')
-		end
-		print('\n')
-	end)
+            print('^0')
+            print('^6[Support Chat]^0 You ^8are not^0 running the newest stable version of ^5Support Chat^0. Please update: https://github.com/Jougito/Support-Chat')
+        else
+            print('^6[Support Chat]^0 Your script is ^2up-to-update^0')
+        end
+        print('^0')
+    end)
 end)
